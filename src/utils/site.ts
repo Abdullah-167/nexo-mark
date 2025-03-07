8484df3c15ba4938484fb031ed2d15cfb662e783
Abdullah-167 <abdullahkhizer007@gmail.com>
2025-03-07 05:18:32 +0500

    commit done

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,5 +1,5 @@
 const site_url =
-  process.env.NEXT_PUBLIC_APP_URL || "https://the-portfolio-lac.vercel.app";
+  process.env.NEXT_PUBLIC_APP_URL || "https://www.nexomark.agency";
 
 export const siteConfig = {
   name: "Nexo Mark",
