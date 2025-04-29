af6e128821b451816d9b49014bc62ba52a0aa47b
Abdullah-167 <abdullahkhizer007@gmail.com>
2025-04-29 12:19:28 +0500

    contact udated

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,14 +1,15 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import ContactUs from "../Components/Contact/ContactUs";
 
-const page = () => {
-
+const Page = () => {
   return (
     <main>
-      <ContactUs />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ContactUs />
+      </Suspense>
     </main>
   );
 };
 
-export default page;
+export default Page;
